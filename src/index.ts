55b053a2d59c1229e387e9122bbeb3974bb5e16a
ddPn08 <pyn.g.1125@gmail.com>
2022-09-30 22:48:09 +0900

    edit package.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import dayjs from 'dayjs'
 import { XMLParser, XMLValidator } from 'fast-xml-parser'
 
 import { ENDPOINTS, HOUJINBANGOU_API_BASE_URL } from './constants'
 import type { diffRequestQuery, nameRequestQuery, numRequestQuery, ResponseData } from './types'
+
+type Option = {
+    version: string
+    customUrl?: string
+}
 
 export class HoujinbangouAPIClient {
     protected zero_pad(num: number): string {
@@ -9,13 +15,24 @@ export class HoujinbangouAPIClient {
     }
 
     private getUrl(endpoint: ENDPOINTS): URL {
-        const url_obj = new URL(HOUJINBANGOU_API_BASE_URL)
-        url_obj.pathname = `/${this.$version}${endpoint}`
-        url_obj.searchParams.set('id', this.$id)
-        return url_obj
+        const url = new URL(this.$option.customUrl || HOUJINBANGOU_API_BASE_URL)
+        url.pathname = `/${this.$option.version}${endpoint}`
+        url.searchParams.set('id', this.$id)
+        return url
     }
 
-    constructor(private $id: string, private $version = '4') {}
+    private parse(text: string) {
+        const isXml = XMLValidator.validate(text)
+        if (isXml == true) {
+            const parser = new XMLParser()
+            const json = parser.parse(text)
+            return json
+        } else {
+            throw new Error(text)
+        }
+    }
+
+    constructor(private $id: string, private $option: Option = { version: '4' }) {}
 
     public async num(query: numRequestQuery): Promise<ResponseData> {
         const url = this.getUrl(ENDPOINTS.Num)
@@ -28,37 +45,17 @@ export class HoujinbangouAPIClient {
         url.searchParams.set('history', `${query.history || '0'}`)
         const res = await fetch(url.toString())
 
-        const parser = new XMLParser()
         const text = await res.text()
-        const isXml = XMLValidator.validate(text)
-        if (isXml == true) {
-            const json = parser.parse(text)
-            return json
-        } else {
-            throw isXml
-        }
+        return this.parse(text)
     }
 
     public async diff(query: diffRequestQuery): Promise<ResponseData> {
         const url = this.getUrl(ENDPOINTS.Diff)
 
-        if (typeof query.kind === 'number') {
+        if (typeof query.kind === 'number')
             query.kind = this.zero_pad(query.kind) as '01' | '02' | '03' | '04'
-        }
-        if (typeof query.from !== 'string') {
-            query.from = [
-                this.zero_pad(query.from.getFullYear()),
-                this.zero_pad(query.from.getMonth()),
-                this.zero_pad(query.from.getDate()),
-            ].join('-')
-        }
-        if (typeof query.to !== 'string') {
-            query.to = [
-                this.zero_pad(query.to.getFullYear()),
-                this.zero_pad(query.to.getMonth()),
-                this.zero_pad(query.to.getDate()),
-            ].join('-')
-        }
+        if (typeof query.from !== 'string') query.from = dayjs(query.from).format('YYYY-MM-DD')
+        if (typeof query.to !== 'string') query.to = dayjs(query.to).format('YYYY-MM-DD')
 
         query.type = '12'
 
@@ -72,15 +69,8 @@ export class HoujinbangouAPIClient {
 
         const res = await fetch(url.toString())
 
-        const parser = new XMLParser()
         const text = await res.text()
-        const isXml = XMLValidator.validate(text)
-        if (isXml == true) {
-            const json = parser.parse(text)
-            return json
-        } else {
-            throw isXml
-        }
+        return this.parse(text)
     }
 
     public async name(query: nameRequestQuery): Promise<ResponseData> {
@@ -88,20 +78,10 @@ export class HoujinbangouAPIClient {
 
         if (typeof query.kind === 'number')
             query.kind = this.zero_pad(query.kind) as '01' | '02' | '03' | '04'
-
-        if (typeof query.from !== 'undefined' && typeof query.from !== 'string')
-            query.from = [
-                this.zero_pad(query.from.getFullYear()),
-                this.zero_pad(query.from.getMonth()),
-                this.zero_pad(query.from.getDate()),
-            ].join('-')
-
-        if (typeof query.to !== 'undefined' && typeof query.to !== 'string')
-            query.to = [
-                this.zero_pad(query.to.getFullYear()),
-                this.zero_pad(query.to.getMonth()),
-                this.zero_pad(query.to.getDate()),
-            ].join('-')
+        if (query.from && typeof query.from !== 'string')
+            query.from = dayjs(query.from).format('YYYY-MM-DD')
+        if (query.to && typeof query.to !== 'string')
+            query.to = dayjs(query.to).format('YYYY-MM-DD')
 
         query.type = '12'
         url.searchParams.set('type', query.type)
@@ -118,14 +98,7 @@ export class HoujinbangouAPIClient {
 
         const res = await fetch(url.toString())
 
-        const parser = new XMLParser()
         const text = await res.text()
-        const isXml = XMLValidator.validate(text)
-        if (isXml == true) {
-            const json = parser.parse(text)
-            return json
-        } else {
-            throw isXml
-        }
+        return this.parse(text)
     }
 }
